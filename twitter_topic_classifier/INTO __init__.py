f8f6b22fe821eae472cb3c50a7943eaca048a99c
Ibrahim Akintunde Akinyera <43181585+akinyeraakintunde@users.noreply.github.com>
2025-11-22 23:10:58 +0000

    Create INTO __init__.py

---
+++ twitter_topic_classifier/INTO __init__.py
@@ -0,0 +1,11 @@
+"""
+Twitter Topic Classifier Package
+--------------------------------
+
+This package provides utilities for training and running a multi-class 
+social media text classifier (Politics, Sports, Entertainment).
+
+Author: Ibrahim Akintunde Akinyera
+"""
+
+from .script_model import predict
